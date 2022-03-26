6dbd0beb7d78ea6ebe50bee05fd623393cbbf1f9
Vasanth Karuppusamy <vasanth.karuppusamy@trigyn.com>
2022-03-26 12:16:59 +0530

    updated the height of each node in a tree while backtracking

--- Algorithms/avlism.py
+++ Algorithms/avlism.py
@@ -1,12 +1,14 @@
 class Node:
-    def __init__(self, _data):
+
+    def __init__(self, data):
         self.left = None
         self.right = None
-        self.data = _data
+        self.data = data
         self.height = 1
 
-class Tree:
-    # creating nodes
+class AVLTree(object):
+
+    # create a Node
     def createNode(self, data):
         return Node(data)
 
@@ -18,5 +20,14 @@ class Tree:
             node.left = self.insert(node.left, data)
         else:
             node.right = self.insert(node.right, data)
-
+        node.height = max(self.getHeight(node.left), self.getHeight(node.right)) + 1
+        print("node ",node.data," height is ",node.height)
         return node
+
+    # get node height
+    def getHeight(self, node):
+        if not node:
+            return 0
+        else:
+            return node.height
+
--- Algorithms/driver.py
+++ Algorithms/driver.py
@@ -1,10 +1,7 @@
 from avlDriver import Tree
 root = None
 tree = Tree()
-datas = [1,2,3,4,5]
+datas = [3,2,4,1,5]
 for data in datas:
     root = tree.insert(root, data)
 print(root)
-root = tree.insert(root, 4)
-root = tree.insert(root, 5)
-root = tree.insert(root, 3)
